67400772708fedfd4212b76c893c339133749cf7
Mike Manchee <mikemanchee@gmail.com>
2020-08-24 19:27:56 -0700

    add js constructor  spec 5 to the project

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,48 @@ ToDoList.prototype.assignId = function() {
   return this.taskId;
 }
 
+ToDoList.prototype.findTask = function(id) {
+  for (let i=0; i< this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (this.tasks[i].id == id) {
+        return this.tasks[i];
+      }
+    }
+  };
+  return false;
+}
 
+ToDoList.prototype.deleteTask = function(id) {
+  for (let i=0; i< this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (this.tasks[i].id == id) {
+        delete this.tasks[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+// addressBook.updateContact(1, 0, 0, 555-666-1212)
+
+ToDoList.prototype.updateTask = function(id, taskNmae, taskDescription, dueDate) {
+  for (let i = 0; i < this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (this.tasks[i].id == id) {
+        if (taskNmae != "") {
+          this.tasks[i].taskNmae = taskNmae;
+        }
+        if (taskDescription != "") {
+          this.tasks[i].taskDescription = taskDescription;
+        }
+        if (dueDate != "") {
+          this.tasks[i].dueDate = dueDate;
+        }
+      }
+    }
+  }
+}
 
 // Business Logic for Tasks
 function Task(taskNmae, taskDescription, phoneNumber) {
